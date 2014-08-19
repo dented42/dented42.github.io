bb3facd820168070586dbdb7cb3a6f7c46ad9a58
Matias Eyzaguirre <matias@fortlogic.net>
2014-08-19 05:42:40 -0600

    external-docs can now depend on each other. mind you, no one is checking for dependency cycles.

--- _src/posts/utils/external-docs.rkt
+++ _src/posts/utils/external-docs.rkt
@@ -27,12 +27,14 @@
               (current-doc-urls)))]))
 
 (define-generics url-ref
-  [ref->url url-ref]
+  [ref-deps url-ref] ; (hashof key name)
+  [ref->url url-ref deps]
   [ref-style url-ref content]
   [api-ref? url-ref]
   #:defaults ([string?
-               (define ref->url identity)])
-  #:fallbacks [(define api-ref? (const #f))
+               (define (ref->url ref deps) ref)])
+  #:fallbacks [(define ref-deps (const (hash)))
+               (define api-ref? (const #f))
                (define (ref-style ref content) content)])
 
 (struct cocoa-class-ref (base class)
@@ -41,25 +43,42 @@
   [(define api-ref? (const #t))
    (define (ref-style ref content)
      (racketidfont content))
-   (define (ref->url ref)
+   (define (ref->url ref deps)
      (apple-url "mac/documentation/"
                 (cocoa-class-ref-base ref)
                 "/"
                 (cocoa-class-ref-class ref)
                 "_Class"))])
 
+(struct independent-reference (ref deps) #:transparent
+  #:methods gen:url-ref
+  [(define/generic super-ref->url ref->url)
+   (define/generic super-ref-style ref-style)
+   (define/generic super-api-ref? api-ref?)
+   (define (ref->url ref deps)
+     (super-ref->url (independent-reference-ref ref) (independent-reference-deps ref)))
+   (define (ref-style ref content)
+     (super-ref-style (independent-reference-ref ref) content))
+   (define (api-ref? ref)
+     (super-api-ref? (independent-reference-ref ref)))])
+
 (define (lookup-ref name (doc-urls (current-doc-urls)))
   (define (lookup urls)
     (cond
       [(null? urls) #f]
       [(hash-has-key? (car urls) name)
-       (hash-ref (car urls) name)]
+       (let ([ref (hash-ref (car urls) name)])
+         (independent-reference ref (flush (ref-deps ref))))]
       [else
        (lookup (cdr urls))]))
+  (define (flush deps)
+    (for/hash ([key (in-hash-keys deps)]
+               [name (in-hash-values deps)])
+      (values key (lookup-ref name doc-urls))))
   (lookup doc-urls))
 
-(define (api name (title name))
+(define (api name (title name) (deps #hash()))
   (let* ([ref (lookup-ref name)])
     (if url
-        (hyperlink (ref->url ref) (ref-style ref title))
+        (hyperlink (ref->url ref deps) (ref-style ref title))
         (TODO title "(broken link)"))))
